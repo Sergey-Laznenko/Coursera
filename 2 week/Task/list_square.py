b1d868a048357f097a7151ef0f22e5506b5e6fe5
Sergey <Laznenko.Sergey@gmail.com>
2020-08-23 11:21:48 +0300

    new

---
+++ 2 week/Task/list_square.py
@@ -0,0 +1,6 @@
+n = int(input())
+score = 1
+
+while score ** 2 <= n:
+    print(score ** 2, end=' ')
+    score += 1
---
+++ 2 week/Task/min_divider.py
@@ -0,0 +1,5 @@
+a = int(input())
+divider = 2
+while a % divider != 0:
+    divider += 1
+print(divider)
