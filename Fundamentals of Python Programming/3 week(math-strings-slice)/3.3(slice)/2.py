bbe483f2c032a293e3f35d6782fd2f12e1fb2636
Sergey <Laznenko.Sergey@gmail.com>
2020-08-30 17:11:47 +0300

    3.3_Task

--- Fundamentals of Python Programming/3 week(math-strings-slice)/3.3(slice)/2.py
+++ Fundamentals of Python Programming/3 week(math-strings-slice)/3.3(slice)/2.py
@@ -1,8 +1,17 @@
+"""
+Дана строка. Если в этой строке буква f встречается только один раз, выведите её индекс.
+Если она встречается два и более раз, выведите индекс её первого и последнего появления.
+Если буква f в данной строке не встречается, ничего не выводите.
+При решении этой задачи нельзя использовать метод count и циклы.
+"""
+
 string = input()
-if string.find('f') != 0:
-    if 'f' not in string:
-        print()
-    elif string.rfind('f') != 0 and string.find('f') != string.rfind('f'):
-        print(string.find('f'), string.rfind('f'))
-    else:
-        print(string.find('f'))
+first = string.find('f')
+revers = string[::-1]
+second = len(string) - revers.find('f') - 1
+if first == second:
+    print(first)
+elif first == -1 or second == -1:
+    print()
+else:
+    print(first, second)
